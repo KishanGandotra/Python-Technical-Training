abddc10b32f812b182a4f6c9bae748d23381cf20
Kishan <156210400+KishanGandotra@users.noreply.github.com>
2024-02-08 17:26:23 +0530

    Delete PROJECT ( CASE STUDY ) CARS PYTHON directory

--- PROJECT ( CASE STUDY ) CARS PYTHON/CASR3/UTIL/PropertyUtil.py
+++
@@ -1,9 +0,0 @@
-class PropertyUtil:
-    @staticmethod
-    def getPropertyString():
-        properties = {}
-        with open('db.properties', 'r') as file:
-            for line in file:
-                key, value = line.strip().split('=')
-                properties[key] = value
-        return properties
